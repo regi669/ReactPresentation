4c443c663d9559487f86ab9516d7468de6c89c4a
Szymon <regicsmaster@gmail.com>
2022-04-22 15:08:46 +0200

    Secont Part Added

--- react-presentation/src/index.js
+++ react-presentation/src/index.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+const store = createStore(() => ({
+  birds: 
+  [
+    {
+      name: 'Jack',
+      views: 15
+    },
+    {
+      name: 'Maister',
+      views: 25
+    },
+    {
+      name: 'Jerry',
+      views: 36
+    }
+  ]
+}));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
     <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
