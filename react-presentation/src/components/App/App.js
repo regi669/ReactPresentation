d4539a40b91e16d1d5cf5cec40f643f61f984c9e
Szymon <regicsmaster@gmail.com>
2022-04-22 15:13:05 +0200

    Added Forms and buttons

--- react-presentation/src/components/App/App.js
+++ react-presentation/src/components/App/App.js
@@ -8,12 +8,26 @@ function App() {
   return (
    <div className="wrapper">
     <h1>Bird List</h1>
+
+    <form>
+      <label>
+        <p>
+          Add Bird
+        </p>
+        <input type="text" />
+      </label>
+      <div>
+        <button type="submit">Add</button>
+      </div>
+    </form>
+
     <ul>
       {birds.map(bird => (
         <li key={bird.name}>
           <h3>{bird.name}</h3>
           <div>
           Views: {bird.views}
+          <button><span role="img" aria-label="add">➕</span></button>
           </div>
         </li>
       ))}
