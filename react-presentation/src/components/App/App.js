68b068302f79c11f6c60576d57d220c6d76ad3bf
Szymon <regicsmaster@gmail.com>
2022-04-26 16:06:30 +0200

    Part3 Update

--- react-presentation/src/components/App/App.js
+++ react-presentation/src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const birds = useSelector(state => state.birds);
+  /*
+  * Next step is to add 1 input form and a button in order for us to
+  * use these components to increment and add new birds in the future*/
   return (
    <div className="wrapper">
     <h1>Bird List</h1>
