3843b68ccedc22184f4ba84ab396d924be50ba3d
Szymon <regicsmaster@gmail.com>
2022-04-26 16:41:41 +0200

    Part6 Update

--- react-presentation/src/components/App/App.js
+++ react-presentation/src/components/App/App.js
@@ -1,22 +1,33 @@
+/*
+* Import useState from react in order to save user input*/
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addBird, incrementBird } from '../../store/birds/birds';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
+  /*
+  * Initialize useState
+  */
   const [birdName, setBird] = useState('');
+  /*
+  * Add a sort function to bird to prevent birds from changing order*/
   const birds = [...useSelector(state => state.birds)].sort((a, b) => {
     return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
   });
+  /*
+  * Create handleSubmit event
+  * to use dispatch to call addBird function in order to
+  * addBird with choosen name*/
   const dispatch = useDispatch();
   const handleSubmit = event => {
     event.preventDefault();
     dispatch(addBird(birdName))
     setBird('');
   };
-
+  /*
+  * Add onChange function to setBird name
+  * and also add onSubmit function to call handleSubmit*/
   return (
    <div className="wrapper">
     <h1>Bird List</h1>
