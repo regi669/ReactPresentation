61761596e44752250c44538bcf6809fce8a62753
Szymon <regicsmaster@gmail.com>
2022-04-26 15:51:19 +0200

    Part1 Update

--- react-presentation/src/index.js
+++ react-presentation/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+
+/*
+* First step is to install react-redux using command
+* npm install --save redux react-redux
+* Then we need to import Provider from react-redux
+* Provider is a main element that wraps our application*/
 import { Provider } from 'react-redux';
 
-
+/*
+* After importing provider we need to wrap out App in it
+* simply by using react Provider Tag*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
